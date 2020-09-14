d290a7327d032eb7994faad43f990525ce13de76
andrewhopgood <andrewhopgood@rogers.com>
2020-09-14 09:42:31 -0400

    initial files and editor/preview windows

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [editorText, setEditorText] = useState("Hi");
+
+  const previewText = (e) => {
+    setEditorText(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <textarea onChange={previewText} id="editor" ></textarea>
+      <div id="preview">{editorText}</div>
     </div>
   );
 }
