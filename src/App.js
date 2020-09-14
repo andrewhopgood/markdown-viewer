b6c17655cfc052220aade8f8fb81087c25a332cd
andrewhopgood <andrewhopgood@rogers.com>
2020-09-14 15:53:00 -0400

    added marked npm, initial useState configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
+import md from './data/md'
 let marked = require("marked");
+
+
 function App() {
 
-  const [editorText, setEditorText] = useState("Hi");
+  const [editorText, setEditorText] = useState(md.data);
+
 
   const previewText = (e) => {
     setEditorText(e.target.value);
@@ -15,7 +18,7 @@ function App() {
     <div className="App">
 
       <textarea onChange={previewText} value={editorText} id="editor" ></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(editorText) }}></div>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(editorText, { breaks: true }) }}></div>
     </div>
   );
 }
---
+++ src/data/md.js
@@ -0,0 +1,44 @@
+export default {
+    "data":
+
+        `# This is a header...oh really?? thanks for letting me know
+## This is a sub-heading...boooooorrrriiinnngg
+### more headers:
+#### ...and more
+
+Check out these sweet p tags, \`<p></p>\`, omg so hot.
+
+#### Time for some lists
+1) You can even make lists 
+-  **What? thats nuts** 
+
+
+ > "Before you judge a man, walk a mile in his shoes.  
+ > After that who cares?... He's a mile away and you've got his shoes!" 
+ >                                          ~ Billy Connolly
+
+ Check out [**freecodecamp.org**](https://www.freecodecamp.com) for awesome material!
+
+Here's some code, oh wow:
+       
+        function markdownViewer(mdText) {
+    if (mdText == true) {
+        return "super cool";
+    }
+} 
+
+**Bold Things**      
+_Slanty Things_   
+~~Crossed Out Things~~   
+~~**_Bolded Crossed Out Slanty Things_**~~
+
+| Tables   | Are       | Cool|
+| :-------:|:---------:|:---:|
+| But      | not       | the |
+| kind     | you       | eat |
+| on       |           |     |   
+
+   
+# Made with React:   
+![React Logo](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)`
+}
